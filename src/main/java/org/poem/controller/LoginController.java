4ed71d2d98885f30118c3664c8982cacb6fa6555
lywworking <xue_2013@sina.com>
2018-10-19 10:34:45 +0800

    登陆接口失败

--- src/main/java/org/poem/controller/LoginController.java
+++ src/main/java/org/poem/controller/LoginController.java
@@ -17,28 +17,28 @@ import javax.servlet.http.HttpServletRequest;
 public class LoginController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-    /**
-     *
-     * @param req
-     * @param model
-     * @return
-     */
-    @RequestMapping(value = "/login")
-    public String showLoginForm(HttpServletRequest req, Model model) {
-        String exceptionClassName = (String)req.getAttribute("shiroLoginFailure");
-        String error = null;
-        if(UnknownAccountException.class.getName().equals(exceptionClassName)) {
-            error = "用户名/密码错误";
-        } else if(IncorrectCredentialsException.class.getName().equals(exceptionClassName)) {
-            error = "用户名/密码错误";
-        } else if(exceptionClassName != null) {
-            error = "其他错误：" + exceptionClassName;
-        }
-        model.addAttribute("error", error);
-        logger.info("error:  "+error);
-
-        // 此方法不处理登陆成功（认证成功），shiro认证成功会自动跳转到上一个请求路径
-        // 登陆失败还到login页面
-        return "user/login";
-    }
+//    /**
+//     *
+//     * @param req
+//     * @param model
+//     * @return
+//     */
+//    @RequestMapping(value = "/login")
+//    public String showLoginForm(HttpServletRequest req, Model model) {
+//        String exceptionClassName = (String)req.getAttribute("shiroLoginFailure");
+//        String error = null;
+//        if(UnknownAccountException.class.getName().equals(exceptionClassName)) {
+//            error = "用户名/密码错误";
+//        } else if(IncorrectCredentialsException.class.getName().equals(exceptionClassName)) {
+//            error = "用户名/密码错误";
+//        } else if(exceptionClassName != null) {
+//            error = "其他错误：" + exceptionClassName;
+//        }
+//        model.addAttribute("error", error);
+//        logger.info("error:  "+error);
+//
+//        // 此方法不处理登陆成功（认证成功），shiro认证成功会自动跳转到上一个请求路径
+//        // 登陆失败还到login页面
+//        return "user/login";
+//    }
 }
